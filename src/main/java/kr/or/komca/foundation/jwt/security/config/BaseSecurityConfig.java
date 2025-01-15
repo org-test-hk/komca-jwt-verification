103f4e022033eff74e2a25a09fda1dad4c1434ff
kun <sus5330@icloud.com>
2025-01-15 16:18:00 +0900

    feat: baseSecurityConfig 수정 (공통 애노테이션 추가 및 filter 구성 추가)

--- src/main/java/kr/or/komca/foundation/jwt/security/config/BaseSecurityConfig.java
+++ src/main/java/kr/or/komca/foundation/jwt/security/config/BaseSecurityConfig.java
@@ -1,14 +1,27 @@
 package kr.or.komca.foundation.jwt.security.config;
 
+import kr.or.komca.foundation.jwt.exception.handler.SecurityExceptionHandler;
+import kr.or.komca.foundation.jwt.security.filter.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 // Common Security Module의 추상 클래스
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public abstract class BaseSecurityConfig {
+
+	private final JwtAuthenticationFilter jwtAuthenticationFilter;
+	private final SecurityExceptionHandler securityExceptionHandler;
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -17,7 +30,11 @@ public abstract class BaseSecurityConfig {
 				.formLogin(AbstractHttpConfigurer::disable)
 				.httpBasic(AbstractHttpConfigurer::disable)
 				.sessionManagement(session ->
-						session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+						session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+				.exceptionHandling(handler ->
+						handler.accessDeniedHandler(securityExceptionHandler)
+								.authenticationEntryPoint(securityExceptionHandler));
 
 		// 각 서비스의 권한 설정 적용
 		configureAuthorization(http);
